82f5a0b9dba1923a407e33c86fe128ba309f1bbb
Judy942 <109392364+Judy942@users.noreply.github.com>
2024-12-03 07:32:47 +0700

    Initial commit

---
+++ src/main/java/kma/javacardproject/Javacardproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package kma.javacardproject;
+
+/**
+ *
+ * @author Judy
+ */
+public class Javacardproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
